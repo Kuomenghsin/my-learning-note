04bf324679b4931ef50258d625724a89014511e6
Kuomenghsin <44611482+Kuomenghsin@users.noreply.github.com>
2019-10-17 20:37:54 +0800

    Update 06111306 HW1.py

--- HW1/06111306 HW1.py
+++ HW1/06111306 HW1.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
 
 arr = [9,11,8,6,5,12,3,1]
+
+
 def quick_sort(list):
     small = []
     big = []
@@ -14,11 +11,11 @@ def quick_sort(list):
         return list
 
     else:
-        key = list[0] #第一個數為key值
+        key = list[0] #第一個數為選取的任意值
         for i in list:
-            if i < key: #比key值小的數
+            if i < key: #比任意值小的數
                 small.append(i)
-            elif i > key: #比key值大的數
+            elif i > key: #比任意值大的數
                 big.append(i)    
             else:
                 point.append(i)
